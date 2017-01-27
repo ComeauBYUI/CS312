844e6ebbc09477900d0e49e6494af95bf50b6292
Luc Comeau <comeaul@byui.edu>
2017-01-27 14:25:06 -0700

    Solution

---
+++ 04TeamB Solution/main.js
@@ -0,0 +1,100 @@
+// Get the canvas object
+var canvas = document.getElementById('canvas');
+
+// Create the CS312 drawing context
+var ctx = new cs312Context(canvas);
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+var gameBoardMin = -canvasWidth / 2;
+var gameBoardMax = canvasHeight / 2;
+
+var rotateLeft = 0;
+var rotateRight = 0;
+
+var scaleFactor = 1;
+var scaleFactorDiff = 0.02
+
+var shearFactor = 0;
+var shearFactorDiff = 1
+
+const BALLCOUNT = 1;    // max is 9 with the current position
+const BALLRADIUS = 50;
+var balls = [];
+
+/*****************************************************************************
+ * move all
+ ****************************************************************************/
+function moveAll() {
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].advance();
+  }
+
+  // bounce
+  for (var i = 0; i < balls.length; i++) {
+    for (var j = i + 1; j < balls.length; j++) {
+      var dist = balls[i].pos.distance(balls[j].pos);
+      var rad = balls[i].getRadius() + balls[j].getRadius();
+      if (dist <= rad) {
+        // Need to bounce the balls
+        var same = (balls[i].getType() == balls[j].getType());
+        balls[i].incHitCount(same);
+        balls[j].incHitCount(same);
+      }
+    }
+  }
+}
+
+/*****************************************************************************
+ * move all
+ ****************************************************************************/
+function drawAll() {
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+  }
+}
+
+/*****************************************************************************
+ * Move the game to the next frame
+ ****************************************************************************/
+function loop() {
+
+  // color in the background
+  ctx.fillStyle = "#000000";
+  ctx.fill(0, 0, canvasWidth, canvasHeight);
+
+  moveAll();
+  drawAll();
+}
+
+/*****************************************************************************
+ * Main - the game
+ ****************************************************************************/
+function main() {
+
+  // create balls
+  for (var i = 0; i < BALLCOUNT; i++)
+  {
+    balls.push(new Ball(ctx,
+              getRandomFloat(-canvasWidth, canvasWidth),
+              getRandomFloat(-canvasHeight, canvasHeight), 1));
+
+    balls.push(new Ball(ctx,
+                        getRandomFloat(-canvasWidth, canvasWidth),
+                        getRandomFloat(-canvasHeight, canvasHeight), 2));
+  }
+
+  // Move the objects to the center
+  ctx.translate(canvasWidth / 2, canvasHeight / 2);
+
+  console.log('canvasWidth = ' + canvasWidth);
+  console.log('canvasHeight = ' + canvasHeight);
+
+  console.log('gameBoardMin = ' + gameBoardMin);
+  console.log('gameBoardMax = ' + gameBoardMax);
+
+  // 30 times a second - note change it if you need to debug your code
+  setInterval(loop, 1000 / 30);
+}
---
+++ 04TeamB Solution/point.js
@@ -0,0 +1,66 @@
+/****************************************************************************
+ * Point Object
+ ***************************************************************************/
+
+function Point(x, y) {
+    this.x = 0.0;
+    this.y = 0.0;
+
+    this.x = x;
+    this.y = y;
+    this.w = 1;
+}
+
+/***************************************************************************
+ * prototype functions
+ ***************************************************************************/
+Point.prototype = {
+
+  constructor: Point,
+
+  copy: function(rhs) {
+    this.x = rhs.x;
+    this.y = rhs.y;
+    this.w = 1;
+  },
+
+  distance: function (b) {
+    var dx = this.x - b.x;
+    var dy = this.y - b.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+
+  addPoint: function (point2) {
+    this.x += point2.x;
+    this.y += point2.y;
+  },
+
+  getX: function() {
+    return this.x;
+  },
+
+  getY: function() {
+    return this.y;
+  },
+
+  applyMatrix: function(mat) {
+    // Calculate new x and y values
+    var x = (mat.v[0][0] * this.x) + (mat.v[0][1] * this.y) + (mat.v[0][2]);
+    var y = (mat.v[1][0] * this.x) + (mat.v[1][1] * this.y) + (mat.v[1][2]);
+
+    // copy those new values to the x and y for this object
+    this.x = x;
+    this.y = y;
+    this.w = 1;
+  },
+
+  log: function (title) {
+    if (title != null) {
+      console.log('Point: ' + title + ' (' + this.x + ', ' + this.y + ', ' + this.w + ')');
+    } else {
+      console.log('Point: (' + this.x + ', ' + this.y + ', ' + this.w + ')');
+    }
+  }
+
+};
